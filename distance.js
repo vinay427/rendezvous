edfabcbbbfaabbeb59ae64580e30270c296198f4
Prakhar Bhandari <prakharb@seas.upenn.edu>
2014-09-06 16:03:31 -0400

    rough draft of function for getting coordinates

--- distance.js
+++ distance.js
@@ -1,3 +1,35 @@
 42.298592, -83.721382
 42.261379, -83.746788
 42.284243, -83.779060
+getDestance()
+
+for x in (1, 100):
+    for y in (1, 100):
+
+
+function gps(address) {
+    var api_key = 'AIzaSyC2Z7FZ2GdFBy_U4vCaEDmJohihq627ia0';
+
+    var text_address = address;
+
+    var coordinates = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address '&key=' + api_key;
+
+    var lat = coordinates.results[0].geometry.location.lat;
+    var lng = coordinates.results[0].geometry.location.lng;
+
+    var point = new pt(lat, lng);
+    return pt;
+}
+
+
+$.getJSON( "ajax/test.json", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
--- main.js
+++ main.js
@@ -5,12 +5,13 @@ function add_fields() {
 	$('#numElements').val(id);
     newspan.innerHTML = '<div class="pure-u-1-2"><input type="text" id=a' + id + ' placeholder="Address ' + id + '"></div><div class="pure-u-1-2"><select><option>Car</option><option>Bike</option><option>Walk</option><option>Pub. Transit</option></select></div>';
     document.getElementById('addresses').appendChild(newspan);
+}
 
-$.getJSON('https://maps.google.com/maps/api/geocode/json?address=milano&sensor=false&key=AIzaSyC2Z7FZ2GdFBy_U4vCaEDmJohihq627ia0',function(data) {
-    var location = data.results[0].geometry.location.lat;
-    console.log(location);
-    // coordinates are location.lat and location.lng
-    });
+function get_coordinates(location) {
+	$.getJSON('https://maps.google.com/maps/api/geocode/json?address=' + location + '&sensor=false&key=AIzaSyC2Z7FZ2GdFBy_U4vCaEDmJohihq627ia0',function(data) {
+		var location = data.results[0].geometry.location;
+		var coordinates = [location.lat, location.lng];
+	});
 }
 
 function add2_fields() {
@@ -19,9 +20,9 @@ function add2_fields() {
 //	document.getElementById('numElements').value = id;
 	$("").appendTo("addresses");
 }
-	
+
 function remove_fields() {
-	
+
 }
 /*
 function point(lat, lng) {
@@ -31,37 +32,37 @@ function point(lat, lng) {
 
 function gps(address) {
 	var api_key = 'AIzaSyC2Z7FZ2GdFBy_U4vCaEDmJohihq627ia0';
-	
+
 	var text_address = address;
-	
+
 	var coordinates = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address '&key=' + api_key;
-	
+
 	var lat = coordinates.results[0].geometry.location.lat;
 	var lng = coordinates.results[0].geometry.location.lng;
-	
+
 	var point = new pt(lat, lng);
 	return pt;
 }
-	
+
 
 function find_point(){
 	alert("hello world");
 
 //	point pts_arr[5];
-	
+
 //	var pts_arr = new point(gps(document.getElementById('a1')));
 //	pts_arr[2] = gps(document.getElementById('a2'));
 //	pts_arr[3] = gps(document.getElementById('a3'));
 //	pts_arr[4] = gps(document.getElementById('a4'));
 //	pts_arr[5] = gps(document.getElementById('a5'));
-	
+
 //	var r_lat = (pts_arr[1].lat + pts_arr[2].lat + pts_arr[3].lat + pts_arr[4].lat + pts_arr[5].lat) / 5;
 //	var r_lng = (pts_arr[1].lng + pts_arr[2].lng + pts_arr[3].lng + pts_arr[4].lng + pts_arr[5].lng) / 5;
-	
+
 //	point result(r_lat, r_lng);
-	
+
 //	alert(result.r_lat);
-	
+
 //	return result;
 }
 
@@ -69,24 +70,24 @@ function find_point(){
 	alert("hello world");
 
 	var pts_arr = new point();
-	
+
 	pts_arr.push(new point());
-/*	
+/*
 	pts_arr[1] = gps(document.getElementById('a1'));
 	pts_arr[2] = gps(document.getElementById('a2'));
 	pts_arr[3] = gps(document.getElementById('a3'));
 	pts_arr[4] = gps(document.getElementById('a4'));
 	pts_arr[5] = gps(document.getElementById('a5'));
-	
+
 	var r_lat = (pts_arr[1].lat + pts_arr[2].lat + pts_arr[3].lat + pts_arr[4].lat + pts_arr[5].lat) / 5;
 	var r_lng = (pts_arr[1].lng + pts_arr[2].lng + pts_arr[3].lng + pts_arr[4].lng + pts_arr[5].lng) / 5;
-	
+
 	var result = new point(r_lat, r_lng);
-	
+
 */
 /*
 	alert("hello 1");
-	
+
 //	return result;
 }
 
@@ -97,16 +98,16 @@ function point(lat, lng) {
 
 function gps(address) {
 	var api_key = 'AIzaSyC2Z7FZ2GdFBy_U4vCaEDmJohihq627ia0';
-	
+
 	var text_address = address;
-	
+
 	var coordinates = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + api_key;
-	
+
 	var lat = coordinates.results[0].geometry.location.lat;
 	var lng = coordinates.results[0].geometry.location.lng;
-	
+
 	var point = new pt(lat, lng);
 	return pt;
 }
 */
-	
+
