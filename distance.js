54387740b928204edc2b08e6f166902aa780cb55
Vinay Hiremath <vinayh@beyster1620p68.engin.umich.edu>
2014-09-07 05:55:24 -0400

    Added nearby place finder

--- distance.js
+++ distance.js
@@ -3,17 +3,20 @@ var service;
 var infowindow;
 
 function initialize() {
-  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
 
-  map = new google.maps.Map(document.getElementById('map'), {
+  var pyrmont = new google.maps.LatLng(42.2814, -83.7483);
+
+	typesArray = placeTypes();
+
+map = new google.maps.Map(document.getElementById('map'), {
       center: pyrmont,
       zoom: 15
     });
 
   var request = {
     location: pyrmont,
-    radius: '500',
-    types: ['store']
+    radius: '5000',
+    types: [typesArray]
   };
 
   service = new google.maps.places.PlacesService(map);
@@ -25,7 +28,16 @@ function callback(results, status) {
     for (var i = 0; i < results.length; i++) {
       var place = results[i];
 	console.log(place.name);
+	console.log(place.website);
       createMarker(results[i]);
     }
   }
+}
+
+
+function placeTypes() {
+	var e = document.getElementById("type");
+	var chosenType = e.options[e.selectedIndex].value;
+	console.log(chosenType);
+	return chosenType;
 }
